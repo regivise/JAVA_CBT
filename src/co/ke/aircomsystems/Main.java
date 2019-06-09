5cfa69330750fe3c1f17730a07f919262c04b724
regivise <reginald.odhiambo@gmail.com>
2019-06-09 23:28:04 +0300

    Add hello add int

--- src/co/ke/aircomsystems/Main.java
+++ src/co/ke/aircomsystems/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello");
+        int a =3;
+
+        System.out.println(a);
     }
 }
